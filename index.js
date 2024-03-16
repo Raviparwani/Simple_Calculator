05a88b68ae99895b010ca0c04ab8cfad4b7a9b7b
Ravi Parwani <159183341+Raviparwani@users.noreply.github.com>
2024-03-17 03:57:12 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,29 @@
+import inquirer from "inquirer";
+const answer = await inquirer.prompt([
+    { message: "Enter first number",
+        type: "number",
+        name: "firstNumber" },
+    { message: "Enter secound number",
+        type: "number",
+        name: "secoundNumber" },
+    { message: "select one of the operations to perform opration",
+        type: "list",
+        name: "operator",
+        choices: ["Addition", "Subtraction", "Multipilication", "Division"], }
+]);
+if (answer.operator === "Addition") {
+    console.log("Your value is", answer.firstNumber + answer.secoundNumber);
+}
+else if (answer.operator === "Subtraction") {
+    console.log("Your value is", answer.firstNumber - answer.secoundNumber);
+}
+else if (answer.operator === "Multipilication") {
+    console.log("Your value is", answer.firstNumber * answer.secoundNumber);
+}
+else if (answer.operator === "Division") {
+    console.log("Your value is", answer.firstNumber / answer.secoundNumber);
+}
+else {
+    console.log("Please Select Valid Operator");
+}
+//console.log(answer);
